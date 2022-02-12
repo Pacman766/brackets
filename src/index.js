bd6dbdb01ffab3a406ac9c9c0e55c9fe42823a3b
pacman766 <3334976@gmail.com>
2022-02-12 11:46:52 +0300

    add with mistakes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const brackets = '[]{}()||';
+  const stack = [];
+
+  for(let bracket in str) {
+    let bracketsIndex = brackets.indexOf(bracket);
+
+    if(bracketsIndex % 2 == 0) {
+      stack.push(bracketsIndex + 1);
+    } else {
+      // do smth
+    }
+  }
+  return true ? true : false;
 }
